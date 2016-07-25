3d06d3fc7166db1d0332b54c889fe6b66b6f2ce4
Zach Boline <zachb@uber.com>
2016-07-24 20:12:00 -0700

    optionals

--- README.md
+++ README.md
@@ -15,8 +15,8 @@ The following **required** functionality is completed:
 
 The following **optional** features are implemented:
 * [ ] Add pull-to-refresh for popular stream with SwipeRefreshLayout
-* [ ] Display a nice default placeholder graphic for each image during loading (read more about Picasso)
-* [ ] For popular movies (i.e. a movie voted for more than 5 stars), the full backdrop image is displayed. Otherwise, a poster image, the movie title, and overview is listed. Use Heterogenous ListViews and use different ViewHolder layout files for popular movies and less popular ones.
+* [x] Display a nice default placeholder graphic for each image during loading (read more about Picasso)
+* [x] For popular movies (i.e. a movie voted for more than 5 stars), the full backdrop image is displayed. Otherwise, a poster image, the movie title, and overview is listed. Use Heterogenous ListViews and use different ViewHolder layout files for popular movies and less popular ones.
 * [ ] Expose details of movie (ratings using RatingBar, popularity, and synopsis) in a separate activity.
 * [ ] Add a rounded corners for the images using the Picasso transformations.
 * [x] Tweak the style improving the UI / UX, play with colors, images or backgr$
@@ -29,6 +29,8 @@ The following **additional** features are implemented:
 
 Here's a walkthrough of implemented user stories:
 
-<img src='movie.gif' title='Video Walkthrough' width='' alt='Video Walkthr$
+<img src='movie.gif' title='Video Walkthrough' width='' alt='Video Walkthrough'/>
 
 GIF created with [LiceCap](http://www.cockos.com/licecap/).
+
+I had some issues once I implemented the popular movie layout on the heterogenous list, notably when scrolling back up to the top, some of the "unpopular" items were being placed re-laid-out into the "popular" layout
--- app/src/main/java/com/codepath/movieapp/adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/codepath/movieapp/adapters/MovieArrayAdapter.java
@@ -21,7 +21,7 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
     private static class ViewHolder {
         TextView title;
         TextView overview;
-        ImageView movieImage;
+        volatile ImageView movieImage;
     }
 
     public MovieArrayAdapter(Context context, List<Movie> movies) {
@@ -33,14 +33,22 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
         Movie movie = getItem(position);
 
         ViewHolder viewHolder;
-
+        int orientation = getContext().getResources().getConfiguration().orientation;
         if (convertView == null) {
             viewHolder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.item_movie, parent, false);
-            viewHolder.title = (TextView) convertView.findViewById(R.id.tvTitle);
-            viewHolder.overview = (TextView) convertView.findViewById(R.id.tvOverview);
-            viewHolder.movieImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+            if(movie.getVoteAverage() > 4.9) {
+                convertView = inflater.inflate(R.layout.item_movie_popular, parent, false);
+                viewHolder.title = (TextView) convertView.findViewById(R.id.tvMovieTitle);
+                viewHolder.overview = (TextView) convertView.findViewById(R.id.tvMovieOverview);
+                viewHolder.movieImage = (ImageView) convertView.findViewById(R.id.ivMovie);
+            } else {
+                convertView = inflater.inflate(R.layout.item_movie, parent, false);
+
+                viewHolder.title = (TextView) convertView.findViewById(R.id.tvTitle);
+                viewHolder.overview = (TextView) convertView.findViewById(R.id.tvOverview);
+                viewHolder.movieImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+            }
             convertView.setTag(viewHolder);
 
         } else {
@@ -52,11 +60,18 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
         viewHolder.title.setText(movie.getTitle());
         viewHolder.overview.setText(movie.getOverview());
 
-        int orientation = getContext().getResources().getConfiguration().orientation;
-        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-            Picasso.with(getContext()).load(movie.getPosterPath()).into(viewHolder.movieImage);
+        if (orientation == Configuration.ORIENTATION_PORTRAIT && movie.getVoteAverage() < 4.9) {
+            Picasso.with(getContext()).load(movie.getPosterPath()).placeholder(R.drawable.film)
+                    .error(R.drawable.film).into(viewHolder.movieImage);
         } else {
-            Picasso.with(getContext()).load(movie.getBackdropPath()).into(viewHolder.movieImage);
+            Picasso.with(getContext()).load(movie.getBackdropPath()).placeholder(R.drawable.film)
+                    .error(R.drawable.film).into(viewHolder.movieImage);
+            if (movie.getVoteAverage() > 4.9) {
+                Picasso.with(getContext()).load(movie.getBackdropPath()).fit().centerCrop().placeholder(R.drawable.film)
+                        .error(R.drawable.film).into(viewHolder
+                        .movieImage);
+            }
+
         }
 
         return convertView;
